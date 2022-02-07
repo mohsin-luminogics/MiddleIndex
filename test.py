50db492fd79f4dfddbd25b31b997850fcb550094
Mohsin Tariq <Mohsin.Tariq@staples.com>
2022-02-07 11:01:38 +0500

    Natively found the center point

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # Time Complexity O(n)
 
 
-def recursion(arr, i, j):
+def middleIndex(arr, i, j):
     if arr[i] == arr[j]:
         if i+1 == j-1:
             print(i+1)
@@ -26,14 +26,14 @@ def recursion(arr, i, j):
     elif arr[i] < arr[j]:
         arr[i + 1] = arr[i] + arr[i + 1]
         i = i+1
-    recursion(arr, i, j)
+    middleIndex(arr, i, j)
 
 
 def main():
-    arr = [1,2,1,0]
+    arr = [1, 2, 1, 0]
     j = len(arr) - 1
     i = 0
-    recursion(arr, i, j)
+    middleIndex(arr, i, j)
 
 
 main()
