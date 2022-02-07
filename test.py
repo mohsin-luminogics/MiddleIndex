a19670d98a59fee3dc311a9b78840bed4d2d0d36
Mohsin Tariq <Mohsin.Tariq@staples.com>
2022-02-07 10:58:53 +0500

    Natively found the center point

--- test.py
+++ test.py
@@ -1,10 +1,39 @@
+# Time Complexity O(n)
 
-arr=[1,2,1,2,1]
-def solution():
-    for i in range(1,len(arr)):
-        if i == len(arr)-1:
+
+def recursion(arr, i, j):
+    if arr[i] == arr[j]:
+        if i+1 == j-1:
+            print(i+1)
+            return
+        if i+1 > j-1:
             print("NO Index Found")
-        if sum(arr[:i]) == sum(arr[i+1:]):
-            return i
-index = solution()
-print(index)
+            return
+        if arr[i+1] == arr[j-1]:
+            arr[j-1] = arr[j] + arr[j-1]
+            j = j-1
+            arr[i + 1] = arr[i] + arr[i + 1]
+            i = i+1
+        elif arr[i+1] > arr[j-1]:
+            arr[j - 1] = arr[j] + arr[j - 1]
+            j = j - 1
+        elif arr[i+1] < arr[j-1]:
+            arr[i + 1] = arr[i] + arr[i + 1]
+            i = i + 1
+    elif arr[i] > arr[j]:
+        arr[j-1] = arr[j] + arr[j-1]
+        j = j-1
+    elif arr[i] < arr[j]:
+        arr[i + 1] = arr[i] + arr[i + 1]
+        i = i+1
+    recursion(arr, i, j)
+
+
+def main():
+    arr = [1,2,1,0]
+    j = len(arr) - 1
+    i = 0
+    recursion(arr, i, j)
+
+
+main()
